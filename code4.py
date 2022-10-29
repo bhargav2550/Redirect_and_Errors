411d6091facd5e6b1bf2c90b6fafa67e55d797e6
Bhargav <106314092+bhargav2550@users.noreply.github.com>
2022-10-29 06:08:39 +0530

    Create code4.py

---
+++ code4.py
@@ -0,0 +1,15 @@
+#python program to demonstrate the errors using abort() in Flask.
+#importing abort 
+from flask import Flask,abort
+#Initialize the flask application
+app=Flask(__name__)
+@app.route('/<uname>')
+def index(uname):
+    if uname[0].isdigit():
+        abort(403)
+    return '<h1>Good Username</h1>'
+
+if __name__== '__main__':
+    app.run()
+
+#this code is contributed by Bhargav Katabathuni.
